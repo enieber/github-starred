e6dd83c41c2ccb47b9faa25ee520559767106bf2
Enieber Cunha <eniebercunha@gmail.com>
2021-05-28 03:18:29 -0300

    feat: add swagger and documentation request

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const fastify = require("fastify");
+
+const plugins = require("./config/plugins");
 const githubRouters = require("./github/routers");
 
 function build(opts = { logger: true }) {
   const app = fastify(opts);
+  plugins(app);
 
   githubRouters.forEach((route, index) => {
     app.route(route);
--- github/routers.js
+++ github/routers.js
@@ -5,6 +5,40 @@ const routes = [
     method: "GET",
     url: "/api/starred/:username",
     handler: controller.getAllRepositories,
+    schema: {
+      params: {
+        type: "object",
+        properties: {
+          username: {
+            type: "string",
+            description: "username of gihub",
+          },
+        },
+      },
+      response: {
+        400: {
+          description: "fail request",
+          type: "object",
+          properties: {
+            status: { type: "boolean" },
+            message: { type: "string" },
+          },
+        },
+        200: {
+          description: "Successful response",
+          type: "object",
+          properties: {
+            status: { type: "boolean" },
+            starred: {
+              type: "array",
+              items: {
+                type: "object",
+              },
+            },
+          },
+        },
+      },
+    },
   },
 ];
 module.exports = routes;
